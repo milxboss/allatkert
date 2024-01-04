ac8571318b42c2abed6fa86b74e50dc3723d4412
illes.milan <illesmilan32@gmail.com>
2024-01-04 13:23:46 +0100

    mukodo

--- allatkert.py
+++ allatkert.py
@@ -1,21 +1,28 @@
-zoo = list()
+def addAnimal(zoo, myName):
+    animal = dict()
+    found = False
+    for a in zoo:
+        if myNameame in a.keys():
+            a[myName] += 1
+            found = True
+            #break      
+        if not found:
+            animal[myName] = 1
+            zoo.append(animal)
+        return zoo
+                
+zoo = [{"kecske": 1}]
+animal = dict()
 
-print("Ez egy állatkert!!!")
+print("Ez egy állatkert.")
 print("Állat hozzáadása (1) - Elvétele (2) - Kilépés (0 )")
 select = None
+
 while select != "0":
     select = input("Mit szeretne tenni? ")
     if select != "0":
         if select == "1":
-            #animal = dict()
             name = input("A dög neve: ")
-            if name not in animal.keys():
-                animal[name] = 1
-                zoo.append(animal)
-            else:
-                for a in zoo:
-                    a[name] += 1
-            animal = dict()
-            
-for a in zoo:
-    print(a)
+            addAnimal(name)   
+                 
+print(f"Második: {zoo}")
